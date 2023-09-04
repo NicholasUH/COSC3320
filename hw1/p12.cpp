7876c40820a0a2bac19a0a8b3f26aad056029a48
Nicholas Tran <nicholastran2002@gmail.com>
2023-09-03 22:19:38 -0500

    Modify: p12.py

---
+++ hw1/p12.cpp
@@ -0,0 +1,72 @@
+/*
+Given an array of positive integers nums and a positive integer k, check whether it is 
+possible to divide nums into sets of k consecutive numbers.
+
+Write a recursive function to solve this problem. Return true if it is possible. 
+Otherwise, return false. The input has 2 lines. The first line is the array of positive 
+integers nums. The second line is the positive integer k.
+*/
+
+// e3fc3919-8de9-4d8d-a274-496b820bc2cf
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+bool divideConsecutive(vector<int>& nums, int k) {
+    if (nums.empty()) {
+        return true;
+    }
+
+
+    if (nums.size() % k != 0) {
+        return false;
+    }
+
+    // Get the smallest number in the array
+    int minNum = *min_element(nums.begin(), nums.end());
+
+    for (int i = 0; i < k; ++i) {
+        
+        auto it = find(nums.begin(), nums.end(), minNum + i);
+
+        if (it != nums.end()) {
+            nums.erase(it); 
+        } else {
+            return false;
+        }
+    }
+
+    return divideConsecutive(nums, k);
+}
+
+int main() {
+    
+    vector<int> nums;
+    int k;
+    string temp;
+    int intTemp;
+
+    getline(cin,temp);
+
+    stringstream ss(temp);
+
+    while(ss >> intTemp){
+        nums.push_back(intTemp);
+    }
+    cin >> k;
+    
+
+    if (divideConsecutive(nums, k)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
+
--- hw1/p2.py
+++ hw1/p2.py
@@ -1,16 +1,36 @@
-# e4b509d7-1a81-4f6f-93d3-2d8d957a00cf
+# eff505c4-7c9f-4730-9705-ec8ba17b7fea
 '''
 Given an array A, write a program to find the max(A[j]-A[i]) where i < j. 
 If max(A[j]-A[i])<0, output 0. The input will start with an integer n, which indicates 
 the length of the given array. The next line will be the array.
 '''
 
+'''
+Variables:
+left = holds the lowest value in array
+right = iterates through lists looking for the highest difference after left pointer
+current_maximum = holds the highest maximum different
+'''
+
+'''
+Logic:
+use a left and right pointer to create a "sliding window", left starts on index 0, right
+starts on index 1
+
+use the right pointer to traverse the array and evaluate each index based on its value
+    -if a new lowest value was found ,move left pointer to where right currently is 
+    and continue traversing
+    -else, see if the current value provides a new maximum difference, if it does, update
+    the current maximum, and continue traversing
+'''
+
+
 def find_maximum_difference(arr,arr_length):
     left, right,current_maximum = 0, 1, 0
     while right < arr_length:
         if arr[left] < arr[right]:
             current_maximum = max(current_maximum, arr[right] - arr[left])
-        else:
+        elif(arr[left] > arr[right]):
             left = right
         right += 1
     return current_maximum
@@ -24,3 +44,4 @@ def main():
 
 
 main()
+
