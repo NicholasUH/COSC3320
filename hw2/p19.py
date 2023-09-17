3a07bdf6e6ef77ba49ae9d165e863edd47e48c1d
Nicholas Tran <nicholastran2002@gmail.com>
2023-09-17 18:18:48 -0500

    Add: p19main.py Modify: p19.py

--- hw2/p19.py
+++ hw2/p19.py
@@ -7,8 +7,6 @@ def merge_sort(arr1, arr2, left, right, points1, points2):
 
         merge_helper(arr1, arr2, left, mid, right, points1, points2)
 
-    return
-
 def count_helper(arr1,arr2):
 
     index = final_score = 0
@@ -23,19 +21,43 @@ def count_helper(arr1,arr2):
     return final_score
 
 def merge_helper(arr1, arr2, left, mid, right,points1, points2):
+
+    size1 = (right - left) + 1
+    size2 = right - mid
+
     leftSubarr1 = arr1[left:mid + 1]
     rightSubarr1 = arr1[mid + 1:right + 1]
 
     leftSubarr2 = arr2[left:mid + 1]
     rightSubarr2 = arr2[mid + 1:right + 1]
 
+
+    i1 = i2 = j1 = j2 = 0
+    index1 = index2 = left
+
+    if i1 < size1 and j1 < size2:
+        points1 += count_helper(leftSubarr2,rightSubarr1)
+        i1 += 1
+
+    if i2 < size1 and j2 < size2:
+        points2 += count_helper(leftSubarr1,rightSubarr2)
+
+
+    #merge back subarrays
+
     
-
-
 
 
 def main():
     arr1 = list(map(int, input().split()))
     arr2 = list(map(int, input().split()))
-    robert_score,rachel_score = merge_sort(arr1,arr2)
-    print(f"{robert_score} {rachel_score}")
+    score1,score2 = 0,0
+    merge_sort(arr1, arr2, 0 , max(len(arr1),len(arr2)) - 1, score1,score2)
+    print(f"{score1} {score2}")
+
+
+main()
+
+
+#5 3 6 9
+#7 5 8 4
