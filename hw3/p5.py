56f3bf7843d9041e53ab3c0f6854122a5b353754
Nicholas Tran <nicholastran2002@gmail.com>
2023-10-01 13:16:23 -0500

    Add: p5.py solution

--- hw3/p5.py
+++ hw3/p5.py
@@ -1,25 +1,30 @@
-# 6a6237c7-a22e-4d8b-a335-1fda5dab5bdf
+# 70665735-ae48-4466-bb72-69f20d215e48
 
-def function(preorder_list,inorder_list):
+def reconstruct_binary_tree(preorder_list,inorder_list):
     if not preorder_list or not inorder_list:
         return []
+    
+    # find root value using preorder
     root = preorder_list[0] 
-    root_index = inorder_list.index(root) 
 
-    left_subtree = function(preorder_list[1:1+root_index], inorder_list[:root_index])
+    # find root index in inorder to determine left and right subtree
+    # everything left is left subtree, likewise for the right subtree
+    root_index = inorder_list.index(root)
 
-    right_subtree = function(preorder_list[1+root_index:], inorder_list[root_index+1:])
+    #create the proper subtrees
+    left_subtree = reconstruct_binary_tree(preorder_list[1:root_index+1], inorder_list[:root_index])
+    right_subtree = reconstruct_binary_tree(preorder_list[root_index+1:], inorder_list[root_index + 1:])
 
+    # return in post-order fashion
     return left_subtree + right_subtree + [root]
 
-
 def main():
-    n = int(input()) 
+    # input parsing
+    num_of_nodes = int(input()) 
     preorder_list = input().split()
     inorder_list = input().split()  
 
-    postorder_list = function(preorder_list, inorder_list)
-
+    postorder_list = reconstruct_binary_tree(preorder_list, inorder_list)
     print(" ".join(map(str, postorder_list)))
 
 main()
